56f4375d72a844a63c7a2d3294c7e18928ee0ae0
bestquangminh <bestquangminh@gmail.com>
2023-08-08 23:26:35 +0700

    add databaseURL from Atlast

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 async function connect() {
   try {
-    await mongoose.connect('mongodb://localhost:27017/node_bookstore');
+    await mongoose.connect(process.env.databaseURL || 'mongodb://localhost:27017/node_bookstore');
     console.log('mongoose connect succeeded');
   } catch (error) {
     console.log('mongoose connect failed');
